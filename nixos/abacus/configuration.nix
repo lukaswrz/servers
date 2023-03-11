a8de70779446b9f9fcb1dc43628ebe5280b422f6
Lukas Wurzinger <lukas@wrz.one>
2023-03-11 12:42:22 +0100

    firewall

--- nixos/abacus/configuration.nix
+++ nixos/abacus/configuration.nix
@@ -17,7 +17,7 @@
     networkmanager.enable = true;
     firewall = {
       enable = true;
-      allowedTCPPorts = [ 22 ];
+      allowedTCPPorts = [ 22 80 443 ];
       allowedUDPPorts = [ ];
     };
   };
